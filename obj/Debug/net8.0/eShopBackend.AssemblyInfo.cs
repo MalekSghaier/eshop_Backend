fbca7ac4eb6a0a07d5889557138677d53840061c
MalekSghaier <melak.seghaier@gmail.com>
2024-06-21 11:16:41 +0200

    first commit

--- obj/Debug/net8.0/eShopBackend.AssemblyInfo.cs
+++ obj/Debug/net8.0/eShopBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eShopBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1470d2ab40a96e177dc5cd627354544c98390e81")]
 [assembly: System.Reflection.AssemblyProductAttribute("eShopBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eShopBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
